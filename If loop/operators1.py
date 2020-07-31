c676c6b0bd0fd879748a1a32ba38029192061f60
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2020-07-31 18:13:52 +0530

    ifloopadded

---
+++ If loop/operators1.py
@@ -0,0 +1,22 @@
+print("Basic arithamatics")
+num1 = float(input("Enter First number ="))
+num2 = float(input("Second number ="))
+print("Enter your desiered operation")
+ad = "Addition"
+sb = "Subtraction"
+ml = "Multiplication"
+dv = "Devition"
+op = input()
+if op == ad:
+    redult = num1 + num2
+    print(redult)
+elif op == sb:
+    result = num1 - num2
+    print(result)
+elif op == ml:
+    result = num1*num2
+    print(result)
+elif op == dv:
+    result = num1/num2
+    print(result)
+else:print("Invalid input")
