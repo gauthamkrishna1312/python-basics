97995c06b4a686fe86b043a63ed7b161e40246de
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2021-05-14 09:21:29 +0530

    Modified Operators

--- If loop/operators1.py
+++ If loop/operators1.py
@@ -1,11 +1,15 @@
 print("Basic arithamatics")
+
 num1 = float(input("Enter First number ="))
 num2 = float(input("Second number ="))
+
 print("Enter your deziered operation")
 ad = "Addition"
 sb = "Subtraction"
 ml = "Multiplication"
 dv = "Devition"
+print(ad,sb,ml,dv)
+
 op = input()
 if op == ad:
     redult = num1 + num2
