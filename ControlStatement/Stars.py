9496bf973edce86edce4b24b41ef038a0549986e
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2021-05-15 14:11:16 +0530

    StarUsingWhileLoop

---
+++ ControlStatement/Stars.py
@@ -0,0 +1,20 @@
+num = int(input("Enter height : "))
+
+row = 0
+while row < num:
+    space = num - row - 1
+    while space > 0:
+        print(end =" ")
+        space = space - 1
+    star = row + 1
+    while star > 0:
+        print("*",end=" ")
+        star = star - 1
+    row = row + 1
+    print()
+else:
+    print("Star Finished")
+
+
+
+
