1fcc4b7ec3524ab661a8fa3c003eed90b07b607d
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2021-05-15 12:56:46 +0530

    Loop1 Added

--- 1/List.py
+++ 1/List.py
@@ -3,3 +3,10 @@ print(Gadgets)
 
 Gadgets[3] = "Mp3 Player"
 print(Gadgets)
+
+print(Gadgets[0])
+print(Gadgets[1])
+print(Gadgets[-1])
+
+gad = Gadgets.append(input("Enter a value\n"))
+print(Gadgets)
---
+++ ControlStatement/if.py
@@ -0,0 +1,10 @@
+print("IF LOOP")
+
+num1 = int(input("Enter an integer = "))
+
+if num1 > 0:
+    print("Entered value is a positive integer")
+elif num1 == 0:
+    print("You are entered zero")
+else:
+    print("Entered value is a negative integer")
